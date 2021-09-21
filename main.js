25ee7d242786bd5205b52a9b3754f717e57945ac
eric guzman <xericguzman@gmail.com>
2021-09-21 07:00:00 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+let myAge = 31;
+let futureAge = myAge + 5;
+console.log('My age in five years will be: ' + futureAge);
+
+let num1 = 4;
+let num2 = 20;
+let sum = num1 + num2;
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + sum);
+
+let myName = 'Eric';
+console.log('Updog, ' + myName);
+
+let forRem = 10;
+let alsoforRem = 100;
+let Rem = alsoforRem % forRem;
+console.log('The remainder of ' + alsoforRem + ' divided by ' + forRem + ' equals ' + Rem);
+
+let x1 = 4;
+let y1 = 3;
+let x2 = 6;
+let y2 = 7;
+let distance= Math.sqrt(x2-x1) + Math.sqrt(y2-y1);
+console.log('The distance between the two coordinates, in miles, is ' + distance);
+
+
